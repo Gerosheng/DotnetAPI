895a0ce8525d0d8dde3247b11336edc16ec43cd1
Jérôme <jerome.coeck@outlook.com>
2024-02-20 15:26:39 +0100

    Password Management hash+salt

--- Dtos/UserForLoginConfirmationDto.cs
+++ Dtos/UserForLoginConfirmationDto.cs
@@ -1,8 +1,8 @@
 namespace DotnetAPI.Dtos
 {
-    partial class UserForLoginConfirmationDto
+    public partial class UserForLoginConfirmationDto
     {
-        byte[] PasswordHash {get; set;} = [];
-        byte[] PasswordSalt {get; set;} = [];
+        public byte[] PasswordHash {get; set;} = [];
+        public byte[] PasswordSalt {get; set;} = [];
     }
 }
