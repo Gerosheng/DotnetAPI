df3c59246cf14b3f6b534cf6ec8d2ccd8c8b5f3f
Jérôme <jerome.coeck@outlook.com>
2024-03-08 17:36:16 +0100

    Login

--- obj/Debug/net8.0/DotnetAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotnetAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2f05da5fa088c22caccd6f62e56503e4572ff05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4ed8da52cacc87a55f33bd67b1c26ae1eaffabb")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
