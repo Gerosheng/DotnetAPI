f2f05da5fa088c22caccd6f62e56503e4572ff05
Jérôme <jerome.coeck@outlook.com>
2024-03-08 16:17:16 +0100

    Post Delete

--- obj/Debug/net8.0/DotnetAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotnetAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6c78656d8b29a89851f1b5d5c19b27d83e0e3be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0eb9e4be08b789714dc2b19bdb887bfd75613911")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
